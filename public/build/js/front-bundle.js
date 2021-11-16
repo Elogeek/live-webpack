862ca39322430f7f2a5df255a62227ef6a53de0e
elogeek <elodiechristin@gmail.com>
2021-11-16 15:07:39 +0100

    course theory first version on webpack

---
+++ public/build/js/front-bundle.js
@@ -0,0 +1,107 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./assets/js/testConsole.js
+/**
+ *  Fct say hello
+ */
+const sayHi = function(who) {
+    console.log(`Hi ${who}`);
+}
+// Ne jms oublie l'export sinon rien ne fonctionnera
+
+
+/**
+ *  With object literal
+ */
+const person = {
+
+    firstname: "Donald",
+    lastname: 'Doe',
+
+    /**
+     * Fct hello to the object
+     */
+    sayHello: function() {
+        console.log(`Hello ${this.firstname} ${this.lastname}`);
+    },
+
+    /**
+     * Fct goodbye to the object
+     */
+    sayBye: function() {
+        console.log(`sayBye ${this.firstname} ${this.lastname}`)
+    }
+
+};
+
+
+
+/**
+ *  With an object instantiable
+ */
+const people = function() {
+
+    this.firstname = "Daisy";
+    this.lastname =  'Doe';
+
+    /**
+     * Fct hello to the object
+     */
+    this.hello =  function() {
+        console.log(`Hello ${this.firstname} ${this.lastname}`);
+    };
+
+    /**
+     * Fct goodbye to the object
+     */
+    this.goodbye = function() {
+        console.log(`sayBye ${this.firstname} ${this.lastname}`)
+    };
+
+};
+
+
+
+/**
+ *  With an class
+ */
+class myClass {
+
+    constructor() {
+        this.firstname = "Dédé";
+        this.lastname = "Doe";
+    }
+    /**
+     * Fct hello to the object
+     */
+    sayHello =  function() {
+        console.log(`Hello ${this.firstname} ${this.lastname}`);
+    };
+
+    /**
+     * Fct goodbye to the object
+     */
+    sayGoodbye = function() {
+        console.log(`sayBye ${this.firstname} ${this.lastname}`)
+    };
+}
+
+
+;// CONCATENATED MODULE: ./assets/front.js
+// first import
+
+
+//import css from "./css/menu.css";
+
+
+
+sayHi("Bubulle Doe !");
+
+person.sayHello();
+person.sayBye();
+
+
+/******/ })()
+;
