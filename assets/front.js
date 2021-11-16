9a1c47f56a7335543b9d59d0e7bf7ee64d34ea37
elogeek <elodiechristin@gmail.com>
2021-11-16 15:15:58 +0100

    course theory first version on webpack

--- assets/front.js
+++ assets/front.js
@@ -1,7 +1,9 @@
 // first import
 import {sayHi} from "./js/testConsole";
 import {person} from "./js/testConsole";
-import css from "./css/front.css";
+import * as $ from "jquery";
+import Chart from 'chart.js/auto';
+
 
 
 sayHi("Bubulle Doe !");
@@ -9,3 +11,22 @@ sayHi("Bubulle Doe !");
 person.sayHello();
 person.sayBye();
 
+
+$('body').css('backgroundColor', 'orange');
+
+let ctx = document.querySelector('#myChart').getContext('2d');
+let graphic = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Purple','Red','Yellow'],
+        datasets: [{
+            label: 'votes',
+            data: [15,3,9],
+            backgroundColor: [
+                'rgba(255,99,132,1)',
+                'rgba(55,199,132,1)',
+                'rgba(5,99,132,1)'
+            ]
+        }]
+    }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,17 @@
 const webpack = require('webpack');
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
+
     // Entry point
     entry: {
-        front: './assets/front.js',
-        admin: './assets/admin.js',
+        front: './front.js',
+        admin: './admin.js',
     },
     // Here out point
     output: {
         path: path.resolve(__dirname, 'public/build/js'),
         filename: "[name]-bundle.js",
-    },
-    module: {
-        // Rules files css
-        rules: [
-            {
-                // tous le code css se trouve dans mon head de ma console avec style-loader
-                test: /\.css$/i,
-                use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                    {
-                        // mtnt le code css ne se retrouve plus dans le head (mais il renvoie le bon chemin dans la console)
-                        loader: "css-loader",
-                        options: {
-                            import: true,
-                            url: true,
-                            sourceMap: true,
-                        }
-                    }
-                ],
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ],
-    },
-    // Importe le css dans un fichier css portant le même nom par défault
-    plugins: [].concat([new MiniCssExtractPlugin({
-        filename: "../css/[name]-bundle.css"
-    })]),
+    }
 
 };
